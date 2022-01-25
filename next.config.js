fe09a6ded28a085a20afc38c5f7ba470fdd532df
Vladimir Shadrov <v.shadrov@electro.cars>
2022-01-25 16:01:09 +0300

    init

--- next.config.js
+++ next.config.js
@@ -1,6 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  webpack: (config) => {
+    config.module.rules = [
+      ...config.module.rules,
+      {
+        test: /\.svg$/,
+        use: ['@svgr/webpack'],
+      },
+    ];
 
-module.exports = nextConfig
+    return config;
+  },
+};
+
+module.exports = nextConfig;
---
+++ prettier.js
@@ -0,0 +1,9 @@
+module.exports = {
+  singleQuote: true,
+  semi: true,
+  trailingComma: 'all',
+  arrowParens: 'avoid',
+  printWidth: 80,
+  endOfLine: 'auto',
+  tabWidth: 2,
+};
