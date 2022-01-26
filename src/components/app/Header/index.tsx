d9639d5b4d81945b40e7d6ecbd36950fb24bee95
Vladimir Shadrov <v.shadrov@electro.cars>
2022-01-26 14:47:26 +0300

    header ui

--- src/components/app/Header/index.tsx
+++ src/components/app/Header/index.tsx
@@ -1,4 +1,6 @@
 import Link from 'next/link';
+import { useRouter } from 'next/router';
+import cx from 'classnames';
 import LogoIcon from './logo.svg';
 
 const navLinks = [
@@ -9,17 +11,22 @@ const navLinks = [
 ];
 
 export const Header = () => {
+  const { pathname } = useRouter();
   return (
-    <header className="flex items-center ify-cjustenter w-full h-24">
-      <div className="w-12 h-12 bg-white rounded-full">
+    <header className="flex items-center ify-cjustenter w-full h-24 justify-between">
+      <div className="w-12 h-12 bg-white rounded-full mr-16 ">
         <LogoIcon />
       </div>
-      <div>line</div>
-      <div className="h-full flex items-center bg-white bg-opacity-5 backdrop-blur-sm space-x-12 uppercase">
+      <div className="h-full flex items-center bg-white bg-opacity-5 backdrop-blur-sm space-x-12 uppercase px-32 links">
         {navLinks.map((link, index) => (
           <Link href={link.href} key={link.name}>
-            <a className="space-x-3 flex items-center text-white tracking-widest hover:text-red-500">
-              <b>{'0' + index}</b>
+            <a
+              className={cx(
+                'space-x-3 flex items-center text-white tracking-[0.16875em] h-full border-b-[3px] border-transparent hover:border-white transition-colors duration-200',
+                link.href == pathname && 'border-white',
+              )}
+            >
+              <p className="font-bold">{'0' + index}</p>
               <p>{link.name}</p>
             </a>
           </Link>
