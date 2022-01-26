cd738db12072e61d48108bea69764fc0502da865
Vladimir Shadrov <v.shadrov@electro.cars>
2022-01-26 18:17:23 +0300

    add leftside of the main page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,8 +2,19 @@ import type { NextPage } from 'next';
 
 const Home: NextPage = () => {
   return (
-    <div>
-      <h1 className="text-6xl">JUPITER & SATURN</h1>
+    <div className="flex pl-28 pb-28 self-end">
+      <div className="flex flex-col space-y-6">
+        <h2 className="text-blue text-semi tracking-widest">
+          SO, YOU WANT TO TRAVEL TO
+        </h2>
+        <h1 className="text-largest text-white">SPACE</h1>
+        <p className="text-blue text-lg whitespace-pre-line">
+          {` Let’s face it; if you want to go to space, you might as well 
+         genuinely go to outer space and not hover kind of on the 
+         edge of it. Well sit back, and relax because we’ll give you a 
+         truly out of this world experience!`}
+        </p>
+      </div>
     </div>
   );
 };
