bd5274360b02fb43835b42002adedfb730ff6540
Vladimir Shadrov <v.shadrov@electro.cars>
2022-01-26 18:24:57 +0300

    add explore button to main page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@ import type { NextPage } from 'next';
 
 const Home: NextPage = () => {
   return (
-    <div className="flex pl-28 pb-28 self-end">
+    <div className="flex px-28 pb-28 self-end justify-between w-full">
       <div className="flex flex-col space-y-6">
         <h2 className="text-blue text-semi tracking-widest">
           SO, YOU WANT TO TRAVEL TO
@@ -14,6 +14,9 @@ const Home: NextPage = () => {
          edge of it. Well sit back, and relax because we’ll give you a 
          truly out of this world experience!`}
         </p>
+      </div>
+      <div className="w-60 h-60 rounded-full bg-white flex items-center tracking-wider justify-center text-center text-medium self-end cursor-pointer text-blue-dark">
+        EXPLORE
       </div>
     </div>
   );
