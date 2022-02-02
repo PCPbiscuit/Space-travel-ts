514f3b9aae1f7e28937b38ae659981e7df7a5530
Vladimir Shadrov <v.shadrov@electro.cars>
2022-02-02 23:13:11 +0300

    add ids

--- src/pages/api/destination/[planet].ts
+++ src/pages/api/destination/[planet].ts
@@ -7,24 +7,28 @@ const spaceObjectsInfo = [
     distance: '384,400 km',
     time: '3 days',
     info: 'See our planet as you’ve never seen it before. A perfect relaxing trip away to help regain perspective and come back refreshed. While you’re there, take in some history by visiting the Luna 2 and Apollo 11 landing sites.',
+    id: 1,
   },
   {
     name: 'mars',
     distance: '225 MIL. km',
     time: '9 months',
     info: 'Don’t forget to pack your hiking boots. You’ll need them to tackle Olympus Mons, the tallest planetary mountain in our solar system. It’s two and a half times the size of Everest!',
+    id: 2,
   },
   {
     name: 'europa',
     distance: '628 MIL. km',
     time: '3 years',
     info: 'The smallest of the four Galilean moons orbiting Jupiter, Europa is a winter lover’s dream. With an icy surface, it’s perfect for a bit of ice skating, curling, hockey, or simple relaxation in your snug wintery cabin.',
+    id: 3,
   },
   {
     name: 'titan',
     distance: '1.6 BIL. km',
     time: '7 years',
     info: 'The only moon known to have a dense atmosphere other than Earth, Titan is a home away from home (just a few hundred degrees colder!). As a bonus, you get striking views of the Rings of Saturn.',
+    id: 4,
   },
 ];
 
