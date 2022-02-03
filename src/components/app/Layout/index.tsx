8fd78a6ee81181e7ab010fa6e669798502e628ec
Vladimir Shadrov <v.shadrov@electro.cars>
2022-02-03 12:59:59 +0300

    add page name to layout

--- src/components/app/Layout/index.tsx
+++ src/components/app/Layout/index.tsx
@@ -1,4 +1,6 @@
 import { FC } from 'react';
+import cx from 'classnames';
+import { useRouter } from 'next/router';
 
 import { Header } from '../';
 
@@ -8,8 +10,10 @@ type Props = {
 };
 
 export const Layout: FC<Props> = ({ title, color, children }) => {
+  const { pathname } = useRouter();
+  const page = pathname.split('/')[1];
   return (
-    <div className="layout py-10 pl-14 relative ">
+    <div className={cx('layout py-10 pl-14 relative', page)}>
       <Header />
       <main className="flex">{children}</main>
     </div>
