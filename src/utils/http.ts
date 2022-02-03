2f165eeb93818edf9d1395c47503fe04dd4091ee
Vladimir Shadrov <v.shadrov@electro.cars>
2022-02-03 13:00:22 +0300

    fix http module

--- src/utils/http.ts
+++ src/utils/http.ts
@@ -1,6 +1,11 @@
+const dev = process.env.NODE_ENV !== 'production';
+
 export const http = {
   async get(path: string) {
-    const res = await fetch(path, {});
+    const res = await fetch(
+      `${dev ? 'http://localhost:3000' : 'https://prod-link.com'}/api/${path}`,
+      {},
+    );
     return await res.json();
   },
 };
