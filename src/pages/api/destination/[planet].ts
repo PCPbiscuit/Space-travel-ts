95ea0f72d56161a4fbccb5d7b21475c784b160ac
Vladimir Shadrov <v.shadrov@electro.cars>
2022-02-24 10:13:15 +0300

    refactor destinations api and add img urls to api. refactor view accordingly

--- src/pages/api/destination/[planet].ts
+++ src/pages/api/destination/[planet].ts
@@ -4,6 +4,7 @@ import type { NextApiRequest, NextApiResponse } from 'next';
 const spaceObjectsInfo = [
   {
     name: 'moon',
+    image: '/moon.png',
     distance: '384,400 km',
     time: '3 days',
     info: 'See our planet as you’ve never seen it before. A perfect relaxing trip away to help regain perspective and come back refreshed. While you’re there, take in some history by visiting the Luna 2 and Apollo 11 landing sites.',
@@ -11,6 +12,7 @@ const spaceObjectsInfo = [
   },
   {
     name: 'mars',
+    image: '/mars.png',
     distance: '225 MIL. km',
     time: '9 months',
     info: 'Don’t forget to pack your hiking boots. You’ll need them to tackle Olympus Mons, the tallest planetary mountain in our solar system. It’s two and a half times the size of Everest!',
@@ -18,6 +20,7 @@ const spaceObjectsInfo = [
   },
   {
     name: 'europa',
+    image: '/europa.png',
     distance: '628 MIL. km',
     time: '3 years',
     info: 'The smallest of the four Galilean moons orbiting Jupiter, Europa is a winter lover’s dream. With an icy surface, it’s perfect for a bit of ice skating, curling, hockey, or simple relaxation in your snug wintery cabin.',
@@ -25,6 +28,7 @@ const spaceObjectsInfo = [
   },
   {
     name: 'titan',
+    image: '/titan.png',
     distance: '1.6 BIL. km',
     time: '7 years',
     info: 'The only moon known to have a dense atmosphere other than Earth, Titan is a home away from home (just a few hundred degrees colder!). As a bonus, you get striking views of the Rings of Saturn.',
@@ -37,6 +41,7 @@ type Planet = {
   distance: string;
   time: string;
   info: string;
+  image: string;
 };
 
 export default function handler(
--- src/pages/destination/[planet].tsx
+++ src/pages/destination/[planet].tsx
@@ -14,19 +14,13 @@ type Celestial = {
   distance: string;
   time: string;
   info: string;
+  image: string;
 };
 
 type Props = {
   celestial: Celestial;
   celestials: Celestial[];
 };
-
-const celestialsPics = [
-  { name: 'moon', value: moon },
-  { name: 'mars', value: mars },
-  { name: 'europa', value: europa },
-  { name: 'titan', value: titan },
-];
 
 export const getStaticProps: GetStaticProps = async context => {
   const celestial = await http.get(`/destination/${context.params?.planet}`);
@@ -46,8 +40,7 @@ export const getStaticPaths: GetStaticPaths = async () => {
   };
 };
 
-const Destination: NextPage<Props> = ({ celestial }) => {
-  const image = celestialsPics.find(p => p.name == celestial.name)?.value;
+const Destination: NextPage<Props> = ({ celestial, celestials }) => {
   return (
     <div className="flex flex-col px-28 pt-20 w-full space-y-16">
       <div className="flex space-x-7 items-center text-semi text-white tracking-widest uppercase">
@@ -55,20 +48,26 @@ const Destination: NextPage<Props> = ({ celestial }) => {
         <h2>Pick your destination</h2>
       </div>
       <div className="flex items-center pl-16 space-x-28 2xl:space-x-56">
-        <Image alt="Celestial object" src={image || ''} objectFit="contain" />
+        <Image
+          alt="Celestial object"
+          src={celestial.image}
+          objectFit="contain"
+          width="445"
+          height="445"
+        />
         <div className="flex flex-col h-full mb-12 w-1/2 2xl:w-1/3">
           <div className="flex space-x-9 text-white text-base uppercase">
-            {celestialsPics.map(pic => (
-              <Link href={`/destination/${pic.name}`} key={pic.name}>
+            {celestials.map(cel => (
+              <Link href={`/destination/${cel.name}`} key={cel.name}>
                 <a
                   className={cx(
                     'py-4 hover:border-white/50 transition-colors duration-200 border-b-[3px] border-transparent tracking-widest cursor-pointer',
-                    celestial.name == pic.name
+                    celestial.name == cel.name
                       ? 'border-white'
                       : 'border-transparent',
                   )}
                 >
-                  {pic.name}
+                  {cel.name}
                 </a>
               </Link>
             ))}
