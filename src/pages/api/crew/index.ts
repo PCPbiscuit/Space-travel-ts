be219033c2d44610d485b859fb822dc99cb3f3e3
Vladimir Shadrov <shadrov@web-creator.ru>
2022-04-11 09:53:43 +0300

    add pictures to api

--- src/pages/api/crew/index.ts
+++ src/pages/api/crew/index.ts
@@ -8,6 +8,7 @@ const crewMembersInfo = [
     description:
       'Douglas Gerald Hurley is an American engineer, former Marine Corps pilot and former NASA astronaut. He launched into space for the third time as commander of Crew Dragon Demo-2.',
     id: 1,
+    image: '/douglas-hurley.png',
   },
   {
     job: 'pilot',
@@ -15,6 +16,7 @@ const crewMembersInfo = [
     description:
       'Mark Richard Shuttleworth is the founder and CEO of Canonical, the company behind the Linux-based Ubuntu operating system. Shuttleworth became the first South African to travel to space as a space tourist.',
     id: 2,
+    image: '/mark-shuttleworth.png',
   },
   {
     job: 'specialist',
@@ -22,6 +24,7 @@ const crewMembersInfo = [
     description:
       'Pilot on the first operational flight of the SpaceX Crew Dragon to the International Space Station. Glover is a commander in the U.S. Navy where he pilots an F/A-18.He was a crew member of Expedition 64, and served as a station systems flight engineer.',
     id: 3,
+    image: '/victor-glover.png',
   },
   {
     job: 'engineer',
@@ -29,6 +32,7 @@ const crewMembersInfo = [
     description:
       'Anousheh Ansari is an Iranian American engineer and co-founder of Prodea Systems. Ansari was the fourth self-funded space tourist, the first self-funded woman to fly to the ISS, and the first Iranian in space. ',
     id: 4,
+    image: '/anousheh-ansari.png',
   },
 ];
 
