e449b4bb7a28f0ee736c4ac3c574b81487476efc
Vladimir <vovashadrov@yandex.ru>
2022-04-17 18:43:12 +0300

    add on mount transform and transition to destination page

--- src/pages/destination/[planet].tsx
+++ src/pages/destination/[planet].tsx
@@ -2,6 +2,7 @@ import type { NextPage, GetStaticPaths, GetStaticProps } from 'next';
 import Link from 'next/link';
 import cx from 'classnames';
 import Image from 'next/image';
+import { useEffect, useState } from 'react';
 
 import { Celestial, currentPlanetInfo, spaceObjectsInfo } from 'src/data';
 
@@ -27,14 +28,25 @@ export const getStaticPaths: GetStaticPaths = async () => {
 };
 
 const Destination: NextPage<Props> = ({ celestial, celestials }) => {
+  const [mounted, setMounted] = useState(false);
+  useEffect(() => {
+    setMounted(true);
+  }, []);
   return (
     <div className="flex flex-col px-28 pt-20 w-full space-y-16">
       <div className="flex space-x-7 items-center text-semi text-white tracking-widest uppercase">
         <h2 className="opacity-25 font-bold">01</h2>
         <h2>Pick your destination</h2>
       </div>
-      <div className="flex items-center pl-16 space-x-28 2xl:space-x-56">
-        <div className="w-1/2">
+      <div className="flex items-center pl-16 space-x-28 2xl:space-x-56 ">
+        <div
+          className={cx(
+            'w-1/2 transform -translate-x-full transition-transform duration-1000',
+            {
+              '!translate-x-0': mounted,
+            },
+          )}
+        >
           <Image
             alt="Celestial object"
             src={celestial.image}
@@ -44,7 +56,12 @@ const Destination: NextPage<Props> = ({ celestial, celestials }) => {
             layout="responsive"
           />
         </div>
-        <div className="flex flex-col h-full mb-12 w-1/2 2xl:w-1/3">
+        <div
+          className={cx(
+            'flex flex-col h-full mb-12 w-1/2 2xl:w-1/3 transform translate-x-full transition-transform duration-1000',
+            { '!translate-x-0': mounted },
+          )}
+        >
           <div className="flex space-x-9 text-white text-base uppercase">
             {celestials.map(cel => (
               <Link href={`/destination/${cel.name}`} key={cel.name}>
